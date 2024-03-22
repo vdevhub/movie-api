d4df533d848423042cdc72d00ba44aba785ba7ba
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-03-22 06:31:22 +0100

    Add 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,74 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Pearl Harbor',
+    year: '2001',
+    director: 'Michael Bay'
+  },
+  {
+    title: 'Suite Française',
+    year: '2014',
+    director: 'Saul Dibb'
+  },
+  {
+    title: 'Crashpoint - 90 Minuten bis zum Absturz',
+    year: '2009',
+    director: 'Thomas Jauch'
+  },
+  {
+    title: 'Armageddon',
+    year: '1998',
+    director: 'Michael Bay'
+  },
+  {
+    title: 'Taken',
+    year: '2008',
+    director: 'Pierre Morel'
+  },
+  {
+    title: 'La bella e la bestia',
+    year: '2014',
+    director: 'Fabrizio Costa'
+  },
+  {
+    title: 'Ein Sommer in Griechenland',
+    year: '2015',
+    director: 'Jorgo Papavassiliou'
+  },
+  {
+    title: 'Schindler\'s List',
+    year: '1993',
+    director: 'Steven Spielberg'
+  },
+  {
+    title: 'Eat, Pray, Love',
+    year: '2010',
+    director: 'Ryan Murphy'
+  },
+  {
+    title: 'Ocean\'s Eleven',
+    year: '2001',
+    director: 'Steven Soderbergh'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movies list!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
