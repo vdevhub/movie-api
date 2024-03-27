eeaebd0b465e0b651b7938a68ef100d7207adfc3
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-03-27 08:22:22 +0100

    Add delete favorite movie route

--- index.js
+++ index.js
@@ -234,6 +234,22 @@ app.put('/users/:id', (req, res) => {
   }
 });
 
+// DELETE Requests
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const id =  req.params.id;
+  const movieTitle =  req.params.movieTitle;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  }
+  else {
+    res.status(400).send('No such user');
+  }
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Error occurred!');
