55232d9d31fff1f71d1152eb49851e57e0ac43b4
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-08-02 08:14:39 +0200

    CORS switch for testing

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Define CORS
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4200', 'https://myflix-vdevhub.netlify.app', 'https://vdevhub.github.io', 'https://github.com'];
-//app.use(cors());
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 //Import requires authentication and authorization modules
 let auth = require('./auth')(app);
