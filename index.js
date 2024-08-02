ab64030087bcca96b634597a77e0c5dfe40d8078
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-08-02 09:07:56 +0200

    Update CORS

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ let allowedOrigins = [
   'https://myflix-vdevhub.netlify.app',
   'https://vdevhub.github.io',
   'https://github.com'];
-app.use(cors());
-// app.use('*', cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+// app.use(cors());
+app.use('*', cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Import requires authentication and authorization modules
 let auth = require('./auth')(app);
